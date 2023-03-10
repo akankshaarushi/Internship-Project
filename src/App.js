cdf9a2a3d3ad1444da99302f43bdd86e925e1694
akankshaarushi <115232645+akankshaarushi@users.noreply.github.com>
2023-03-11 00:04:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Profile from './Components/Profile';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Profile/>
+    </div>
+  );
+}
+
+export default App;
